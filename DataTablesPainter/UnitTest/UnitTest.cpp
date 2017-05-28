c554a65bc310e894c65a5a5162726281cd1f4d24
BarryPaneer <barry.paneer@qq.com>
2017-05-28 23:35:49 +0800

    updates

--- DataTablesPainter/UnitTest/UnitTest.cpp
+++ DataTablesPainter/UnitTest/UnitTest.cpp
@@ -1,4 +1,6 @@
 #include "UnitTest.h"
+#include "shellapi.h"
+#include "windows.h"
 #include <string>
 #include <iostream>
 #include <algorithm>
@@ -31,12 +33,7 @@ void TestTableOperation::TearDownTestCase()
 
 void TestTableOperation::SetUp()
 {
-	for( int n = 0; n < 3; n++ )
-	{
-		ASSERT_EQ( true, UnitTestEnv::GetDatabasePtr()->CreateTable( T_Message_MarketInfo::GetID(), sizeof(T_Message_MarketInfo), 32 ) );
-		ASSERT_EQ( true, UnitTestEnv::GetDatabasePtr()->CreateTable( T_Message_NameTable::GetID(), sizeof(T_Message_NameTable), 32 ) );
-		ASSERT_EQ( true, UnitTestEnv::GetDatabasePtr()->CreateTable( T_Message_SnapTable::GetID(), sizeof(T_Message_SnapTable), 32 ) );
-	}
+	TestCreateAllTable();
 
 	if( true == m_vctMarketInfo.empty() )
 	{
@@ -101,6 +98,16 @@ void TestTableOperation::SetUp()
 
 void TestTableOperation::TearDown()
 {
+}
+
+void TestTableOperation::TestCreateAllTable()
+{
+	for( int n = 0; n < 3; n++ )
+	{
+		ASSERT_EQ( true, UnitTestEnv::GetDatabasePtr()->CreateTable( T_Message_MarketInfo::GetID(), sizeof(T_Message_MarketInfo), 32 ) );
+		ASSERT_EQ( true, UnitTestEnv::GetDatabasePtr()->CreateTable( T_Message_NameTable::GetID(), sizeof(T_Message_NameTable), 32 ) );
+		ASSERT_EQ( true, UnitTestEnv::GetDatabasePtr()->CreateTable( T_Message_SnapTable::GetID(), sizeof(T_Message_SnapTable), 32 ) );
+	}
 }
 
 void TestTableOperation::TestUpdateMarketInfo( unsigned int nSeed, bool bIsExist )
@@ -370,291 +377,9 @@ void TestTableOperation::TestSelectSnapTable( unsigned int nSeed, bool bIsExist 
 	}
 }
 
-
-I_Message::I_Message( unsigned int nMessageID, unsigned int nMessageLength )
- : m_nCurDataPos( 0 ), m_nMessageID( nMessageID ), m_nMessageLength( nMessageLength )
+void TestTableOperation::TestDeleteAllTables()
 {
-}
-
-unsigned int I_Message::MessageID()
-{
-	return m_nMessageID;
-}
-
-unsigned int I_Message::MessageLength()
-{
-	return m_nMessageLength;
-}
-
-void I_Message::Reset()
-{
-	m_nCurDataPos = 0;
-}
-
-NameTable4CodeTest::NameTable4CodeTest()
- : I_Message( 1234, sizeof(T_Message1234_NameTable) )
-{
-	if( m_vctMessage.capacity() == 0 )
-	{
-		m_vctMessage.reserve( 128 );
-
-		m_vctMessage.push_back( T_Message1234_NameTable( "SR1831930", "中文商品名称1", 123 ) );
-		m_vctMessage.push_back( T_Message1234_NameTable( "m2075-C-3000", "中文商品名称2", 100 ) );
-		m_vctMessage.push_back( T_Message1234_NameTable( "m2075-P-9000", "商品名称3", 200 ) );
-		m_vctMessage.push_back( T_Message1234_NameTable( "IH2075-C-3000", "中文商品名称4", 300 ) );
-		m_vctMessage.push_back( T_Message1234_NameTable( "SR1001930", "商品名称5", 125 ) );
-		m_vctMessage.push_back( T_Message1234_NameTable( "SR1231930", "中文商品名称6", 126 ) );
-	}
-}
-
-bool NameTable4CodeTest::MoveNext()
-{
-	if( 0 == m_vctMessage.size() )	{	return false;	}
-	++m_nCurDataPos;
-
-	if( m_nCurDataPos >= m_vctMessage.size() )	{
-		m_nCurDataPos = 0;
-		return false;
-	}
-
-	return true;
-}
-
-char* NameTable4CodeTest::MessagePointer()
-{
-	T_Message1234_NameTable&		refData = m_vctMessage[m_nCurDataPos];
-	static	unsigned int			sInt = 0;
-
-	refData.SecurityType = sInt++;
-
-	return (char*)(&refData);
-}
-
-SnapTable4ValueTest::SnapTable4ValueTest()
- : I_Message( 2003, sizeof(T_Message2003_SnapTable) )
-{
-	if( m_vctMessage.capacity() == 0 )
-	{
-		m_vctMessage.reserve( 128 );
-
-		m_vctMessage.push_back( T_Message2003_SnapTable( "SR1831930", 9100, 12000, 8000, 123456.011, 987655443 ) );
-		m_vctMessage.push_back( T_Message2003_SnapTable( "m2075-C-3000", 2000, 3000, 1000, 8723.001, 8893240 ) );
-		m_vctMessage.push_back( T_Message2003_SnapTable( "m2075-P-9000", 10000, 12000, 9000, 324.002, 988765 ) );
-		m_vctMessage.push_back( T_Message2003_SnapTable( "IH2075-C-3000", 100, 200, 50, 123209.1, 93024 ) );
-		m_vctMessage.push_back( T_Message2003_SnapTable( "SR1001930", 5000, 5600, 4700, 3242515.106, 899912312 ) );
-		m_vctMessage.push_back( T_Message2003_SnapTable( "SR1231930", 6100, 8000, 5600, 32432.12, 9121211 ) );
-	}
-}
-
-bool SnapTable4ValueTest::MoveNext()
-{
-	if( 0 == m_vctMessage.size() )	{	return false;	}
-	++m_nCurDataPos;
-
-	if( m_nCurDataPos >= m_vctMessage.size() )	{
-		m_nCurDataPos = 0;
-		return false;
-	}
-
-	return true;
-}
-
-char* SnapTable4ValueTest::MessagePointer()
-{
-	T_Message2003_SnapTable&		refData = m_vctMessage[m_nCurDataPos];
-	static	unsigned int			sInt = 0;
-
-	refData.Now = sInt++;
-
-	return (char*)(&refData);
-}
-
-VariableWidthTable4MsgIDTest::VariableWidthTable4MsgIDTest()
- : I_Message( 0, sizeof(T_MessageX_Table) )
-{
-	if( m_vctMessage.capacity() == 0 )
-	{
-		m_vctMessage.reserve( 132 );
-
-		for( int n = 0; n < 3; n++ )
-		{
-			char			pszCode[64] = { 0 };
-
-			::sprintf( pszCode, "m2017-C-%u", n*100 );
-			m_vctMessage.push_back( T_MessageX_Table( pszCode, n*218 ) );
-		}
-	}
-}
-
-unsigned int VariableWidthTable4MsgIDTest::MessageID()
-{
-	unsigned int	nPos = m_nCurDataPos;
-
-	if( m_nCurDataPos >= m_vctMessage.size() )
-	{
-		nPos = 0;
-	}
-
-	T_MessageX_Table&	refData = m_vctMessage[nPos];
-
-	return refData.MsgID;
-}
-
-bool VariableWidthTable4MsgIDTest::MoveNext()
-{
-	if( 0 == m_vctMessage.size() )	{	return false;	}
-	++m_nCurDataPos;
-
-	if( m_nCurDataPos >= m_vctMessage.size() )	{
-		m_nCurDataPos = 0;
-		return false;
-	}
-
-	return true;
-}
-
-char* VariableWidthTable4MsgIDTest::MessagePointer()
-{
-	T_MessageX_Table&		refData = m_vctMessage[m_nCurDataPos];
-	static	unsigned int	sInt = 0;
-
-	refData.Value = sInt++;
-
-	return (char*)(&refData);
-}
-
-void TestAnyMessage_ID_X::SetUpTestCase()
-{
-}
-
-void TestAnyMessage_ID_X::TearDownTestCase()
-{
-}
-
-void TestAnyMessage_ID_X::TestCreateAllTable()
-{
-	unsigned int		nCount = m_vctIMessage.size();
-
-	for( unsigned int n = 0; n < nCount; n++ )
-	{
-		I_Message*			pMessage = m_vctIMessage[n];
-
-		do
-		{
-			ASSERT_EQ( true, UnitTestEnv::GetDatabasePtr()->CreateTable( pMessage->MessageID(), pMessage->MessageLength(), 32 ) );
-		} while( true == pMessage->MoveNext() );
-	}
-}
-
-void TestAnyMessage_ID_X::TestLocateTable( unsigned int nBindID, I_Table** pTable, bool bIsExist )
-{
-	*pTable = UnitTestEnv::GetDatabasePtr()->QueryTable( nBindID );
-
-	if( true == bIsExist )
-	{
-		ASSERT_NE( *pTable, (I_Table*)NULL );			///< 返回数据表指针不能为空
-	}
-	else
-	{
-		ASSERT_EQ( *pTable, (I_Table*)NULL );			///< 返回数据表指针不能为空
-	}
-}
-
-void TestAnyMessage_ID_X::TestInsert1Table( I_Message* pMessage, bool IsExist, int nAffectNum )
-{
-	I_Table*			pTable = NULL;
-
-	if( false == IsExist )
-	{
-		ASSERT_EQ( pTable, (I_Table*)NULL );
-		return;
-	}
-
-	TestLocateTable( pMessage->MessageID(), &pTable, true );
-	ASSERT_NE( pTable, (I_Table*)NULL );
-
-	do
-	{
-		unsigned __int64	nSerialNo = 0;
-		char				pszTmpData[512] = { 0 };
-
-		::memcpy( pszTmpData, pMessage->MessagePointer(), pMessage->MessageLength() );
-		ASSERT_EQ( nAffectNum, pTable->InsertRecord( pszTmpData, pMessage->MessageLength(), nSerialNo ) );
-
-		RecordBlock	record = pTable->SelectRecord( pszTmpData, pMessage->MessageLength() );
-		ASSERT_NE( true, record.IsNone() );				///< 返回数据表指针不能为空
-		ASSERT_EQ( true, record.Compare( RecordBlock(pszTmpData, pMessage->MessageLength()) ) );
-	} while( true == pMessage->MoveNext() );
-}
-
-void TestAnyMessage_ID_X::TestUpdate1Table( I_Message* pMessage, bool IsExist, int nAffectNum )
-{
-	I_Table*			pTable = NULL;
-
-	TestLocateTable( pMessage->MessageID(), &pTable, IsExist );
-
-	if( false == IsExist )
-	{
-		ASSERT_EQ( pTable, (I_Table*)NULL );
-		return;
-	}
-
-	do
-	{
-		unsigned __int64	nSerialNo = 0;
-		char				pszTmpData[512] = { 0 };
-
-		::memcpy( pszTmpData, pMessage->MessagePointer(), pMessage->MessageLength() );
-		ASSERT_EQ( nAffectNum, pTable->UpdateRecord( pszTmpData, pMessage->MessageLength(), nSerialNo ) );
-
-		RecordBlock	record = pTable->SelectRecord( pszTmpData, pMessage->MessageLength() );
-		if( 0 != nAffectNum )	{
-			ASSERT_NE( true, record.IsNone() );				///< 返回数据表指针不能为空
-			ASSERT_EQ( true, record.Compare( RecordBlock(pszTmpData, pMessage->MessageLength()) ) );
-		}
-	} while( true == pMessage->MoveNext() );
-}
-
-void TestAnyMessage_ID_X::TestQuery1Table( I_Message* pMessage, bool IsExist )
-{
-	I_Table*			pTable = NULL;
-	TestLocateTable( pMessage->MessageID(), &pTable, IsExist );
-
-	RecordBlock	record = pTable->SelectRecord( pMessage->MessagePointer(), pMessage->MessageLength() );
-
-	if( true == IsExist )
-	{
-		ASSERT_NE( true, record.IsNone() );				///< 返回数据表指针不能为空
-		ASSERT_EQ( true, record.Compare( RecordBlock(pMessage->MessagePointer(), pMessage->MessageLength()) ) );
-	}
-	else
-	{
-		ASSERT_NE( false, record.IsNone() );			///< 返回数据表指针不能为空
-		ASSERT_EQ( false, record.Compare( RecordBlock(pMessage->MessagePointer(), pMessage->MessageLength()) ) );
-	}
-}
-
-void TestAnyMessage_ID_X::TestDeleteAllTables()
-{
-	UnitTestEnv::GetDatabasePtr()->DeleteTables();
-}
-
-void TestAnyMessage_ID_X::SetUp()
-{
-	if( m_vctIMessage.size() == 0 )						///< 注册所有用到的消息类型
-	{
-		static	NameTable4CodeTest					s_objNameTable4CodeTest;
-		static	SnapTable4ValueTest					s_objSnapTable4ValueTest;
-		static	VariableWidthTable4MsgIDTest		s_objVariableWidthMsg4MsgIDTest;
-
-		m_vctIMessage.push_back( &s_objVariableWidthMsg4MsgIDTest );
-		m_vctIMessage.push_back( &s_objNameTable4CodeTest );
-		m_vctIMessage.push_back( &s_objSnapTable4ValueTest );
-	}
-}
-
-void TestAnyMessage_ID_X::TearDown()
-{
+	ASSERT_EQ( 0, UnitTestEnv::GetDatabasePtr()->DeleteTables() );
 }
 
 
@@ -672,6 +397,8 @@ TEST_F( TestTableOperation, DumpEmptyTablesAndLoad )
 {
 	::printf( "\n **************** 执行测试前, 先清空DataRecover目录 ******************* \n" );
 	::Sleep( 3000 );
+	::system( "del /f/s/q .\\DataRecover\\*" );
+
 	ASSERT_EQ( UnitTestEnv::GetDatabasePtr()->SaveToDisk( "./DataRecover/" ), false );
 	ASSERT_EQ( UnitTestEnv::GetDatabasePtr()->LoadFromDisk( "./DataRecover/" ), false );
 
@@ -897,7 +624,7 @@ TEST_F( TestTableOperation, DumpAllDataAndLoadAfterDeleteSomeRecords )
 }
 
 ///< 创建一堆数据库对象
-TEST_F( TestAnyMessage_ID_X, CreateBundleOfDatabasePointer )
+TEST_F( TestTableOperation, CreateBundleOfDatabasePointer )
 {
 	for( int n = 0; n < 5; n++ )	{
 		ASSERT_NE( GetFactoryObject().GrapDatabaseInterface(), (I_Database*)NULL );
@@ -905,11 +632,8 @@ TEST_F( TestAnyMessage_ID_X, CreateBundleOfDatabasePointer )
 }
 
 ///< 对一数据库进行数据表的创建删除重复测试
-TEST_F( TestAnyMessage_ID_X, CreateDeleteAllTablesTest )
+TEST_F( TestTableOperation, CreateDeleteAllTablesTest )
 {
-	I_Table*			pTable = NULL;
-	unsigned int		nCount = m_vctIMessage.size();
-
 	///< 创建所有数据表
 	TestCreateAllTable();
 	///< 创建所有数据表
@@ -920,7 +644,9 @@ TEST_F( TestAnyMessage_ID_X, CreateDeleteAllTablesTest )
 	TestDeleteAllTables();
 	///< 随意定义一个不存在的数据表
 	::printf( "query table in empty database.........................................\n" );
-	for( unsigned int i = 0; i < nCount; i++ )	{	TestLocateTable( m_vctIMessage[i]->MessageID(), &pTable, false );	}
+	TestLocateMarketInfo( false );
+	TestLocateNameTable( false );
+	TestLocateSnapTable( false );
 	::printf( "happy ending..........................................................\n" );
 	for( int a = 0; a < 10; a++ ) {
 		///< 再次，创建所有数据表
@@ -942,29 +668,10 @@ TEST_F( TestAnyMessage_ID_X, CreateDeleteAllTablesTest )
 	}
 	///< 随意定义一个不存在的数据表
 	::printf( "query table in empty database.........................................\n" );
-	for( unsigned int ii = 0; ii < nCount; ii++ )	{	TestLocateTable( m_vctIMessage[ii]->MessageID(), &pTable, false );	}
+	TestLocateMarketInfo( false );
+	TestLocateNameTable( false );
+	TestLocateSnapTable( false );
 	::printf( "happy ending..........................................................\n" );
-}
-
-///< 对空数据表作记录空更新操作测试
-TEST_F( TestAnyMessage_ID_X, UpdateBeforeInsert )
-{
-	I_Table*			pTable = NULL;
-	unsigned int		nCount = m_vctIMessage.size();
-
-	///< 清空所有数据库
-	TestDeleteAllTables();
-	///< 创建所有数据表
-	TestCreateAllTable();
-	///< 随意定义一个存在的数据表
-	for( unsigned int j = 0; j < nCount; j++ )	{
-		TestLocateTable( m_vctIMessage[j]->MessageID(), &pTable, true );
-	}
-
-	///< 各数据表记录更新循环
-	for( unsigned int m = 0; m < nCount; m++ )	{
-		TestUpdate1Table( m_vctIMessage[m], true, 0 );
-	}
 }
 
 
